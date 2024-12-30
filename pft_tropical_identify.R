1251612388709908d930748cf6aa094553383f2c
mareikeWinkler <mareike.winkler@campus.lmu.de>
2024-12-30 13:26:28 +0100

    commit

--- pft_tropical_identify.R
+++ pft_tropical_identify.R
@@ -23,9 +23,9 @@ lai_trop = transform(lai_trop, to = "lon_lat")          # räumliches
 lai_trop = transform(lai_trop, to = "year_month_day")   # zeitliches
 
 #lai_trop = subset(lai_trop, lat = as.character(seq(-23.25,23.25,0.5)))     # nur der Tropenbereich
-lai_trop = subset(lai_trop, year = as.character(c(2006)), month = as.character(c(7)), band = as.character("tropical broadleaved evergreen tree"))    # nur 2006
+lai_trop = subset(lai_trop, year = as.character(c(2006)), month = as.character(c(7)), band = as.character("tropical broadleaved raingreen tree"))    # nur 2006
 
-plot(lai_trop, main = "LAI of 'tropical broadleaved evergreen tree' [m^2/m^2]")
+plot(lai_trop, main = "LAI of 'tropical broadleaved raingreen tree' [m^2/m^2]")
 
 
 ## Entwurf 2  (VegC)
@@ -35,9 +35,9 @@ vegc_trop = transform(vegc_trop, to = "lon_lat")          # räumliches
 vegc_trop = transform(vegc_trop, to = "year_month_day")   # zeitliches
 
 #lai_trop = subset(vegc_trop, lat = as.character(seq(-23.25,23.25,0.5)))     # nur der Tropenbereich
-vegc_trop = subset(vegc_trop, year = as.character(c(2006)), band = as.character("tropical broadleaved evergreen tree"))    # nur 2006
+vegc_trop = subset(vegc_trop, year = as.character(c(2006)), band = as.character("tropical broadleaved raingreen tree"))    # nur 2006
 
-plot(vegc_trop, main = "VegC of 'tropical broadleaved evergreen tree' [gC/m^2]")
+plot(vegc_trop, main = "VegC of 'tropical broadleaved raingreen tree' [gC/m^2]")
 
 
 ###################################################################################
